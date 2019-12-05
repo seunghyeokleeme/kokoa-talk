5f0c2bc5107200bfc6389f680447715c254f7ebf
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-12-05 16:40:35 +0900

    Routers

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import globalRouter from "./routers/globalRouter";
+import chatRouter from "./routers/chatRouter";
+import friendRouter from "./routers/friendRouter";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/friend", friendRouter);
+app.use("/chat", chatRouter);
+
+export default app;
